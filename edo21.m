2827dfad95b25b1a75cd76851a86eec021ff55b5
Vinícius W <me@vynwg.com>
2025-09-23 12:47:13 -0300

    AAAAAAAAAAAAAAAAAAAAAAA

--- edo21.m
+++ edo21.m
@@ -12,39 +12,32 @@ ha = 0.5;
 hb = 0.25;
 hc = 0.01;
 
-% N1: Número de pontos na partição/discretização (definido em função de a, b e h).
-% N2: Número de pontos para plotar o gráfico da solução analítica exata. N2 >> N1, para que a aparência da curva fique mais suave. 
+N1a = (b - a)/ha;
+N1b = (b - a)/hb;
+N1c = (b - a)/hc;
+N2  = 10*(b - a)/h;
 
-N1 = (b - a)/h; 
-N2 = 10*(b - a)/h;
+t1a = linspace(a,b,N1a+1);
+t1b = linspace(a,b,N1b+1);
+t1c = linspace(a,b,N1c+1);
+t2  = linspace(a,b,N2+1);
 
-% Criação dos vetores t1 e t2 com a discretização do domínio temporal (para a solução aproximada e a exata, respectivamente). 
-
-t1 = linspace(a,b,N1+1);
-t2 = linspace(a,b,N2+1);
-
-% Inicialização dos vetores com a condição inicial. O vetor phi armazena a solução exata; o vetor y, a solução aproximada pelo Método de Runge-Kutta. Editar o valor 1, se necessário.
-
-phi(1) = 1;
-y(1) = 1;
-
-% Geração da solução exata (vetor phi). Calcular à mão e editar as expressões de acordo com o exercício. 
+phi(1) = y0;
+ya(1)  = y0;
+yb(1)  = y0;
+yc(1)  = y0;
 
 for i = 1:N2
-    phi(i+1) = 2*t2(i+1) + 2* exp(-t2(i+1)) - 1;
+    phi(i+1) = 2*t2(i+1) - 1 + 2*exp(-t2(i+1));
 end
 
-% Geração da solução aproximada y pelo Método de Runge-Kutta. Editar as expressões K1, K2, K3 e K4 de acordo com o exercício.
-
-for i = 1:N1
-    K1 = 1 + 2*t1(i) - y(i);        
+for i = 1:N1a
+    K1 = 1 + 2*t1a(i) - ya(i);        
     K2 = 1 + 2*(t1(i)+0.5*h) - (y(i)+0.5*h*K1);
     K3 = 1 + 2*(t1(i)+0.5*h) - (y(i)+0.5*h*K2);
     K4 = 1 + 2*(t1(i)+h) - (y(i)+h*K3);
-    y(i+1) = y(i) + (K1 + 2*K2 + 2*K3 + K4)*h/6; % Linha não-editável.
+    y(i+1) = y(i) + (K1 + 2*K2 + 2*K3 + K4)*h/6;
 end
-
-% Plot da solução aproximada e da exata. 
 
 plot(t2, phi,"-r", t1, y, "ob", "markersize", 3)
 title('Solução exata - (vermelha); Solução aproximada o (azul)')
